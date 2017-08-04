d63ce7d2efd47a9c0f4a48bb72052e7c0ea633b3
pedroguarderas <pedro.guarderas@webrisko.com>
2017-08-04 15:53:38 -0500

    Removing tabs

--- src/distance.hpp
+++ src/distance.hpp
@@ -34,76 +34,76 @@ char ( &_ArraySizeHelper( T (&arr)[N] ))[N];
 
 template< size_t N >
 double dst_weighted( const double (&x)[N], const double (&y)[N],
-		const double (&w)[N], const double (&p)[N] ) {
+    const double (&w)[N], const double (&p)[N] ) {
 
-	double d = 0.0;
-	int size_x = countof( x );
-	int size_y = countof( y );
-	int size_w = countof( w );
-	int size_p = countof( p );
+  double d = 0.0;
+  int size_x = countof( x );
+  int size_y = countof( y );
+  int size_w = countof( w );
+  int size_p = countof( p );
 
-	if ( size_x > 0 && size_x == size_y && size_y == size_w && size_w == size_p ) {
-		int i;
-		for( i = 0; i < size_x ; i++ ) {
-			d += w[i] * pow( abs( x[i] - y[i] ), p[i] );
-		}
-	}
-	return d;
+  if ( size_x > 0 && size_x == size_y && size_y == size_w && size_w == size_p ) {
+    int i;
+    for( i = 0; i < size_x ; i++ ) {
+      d += w[i] * pow( abs( x[i] - y[i] ), p[i] );
+    }
+  }
+  return d;
 }
 
 double dst_weighted( const int& size, double* x, double* y,
-		double* w, double* p ) {
+    double* w, double* p ) {
 
-	double d = 0.0;
-	int i;
+  double d = 0.0;
+  int i;
 
-	for( i = 0; i < size ; i++ ) {
-		if ( (x + i) == nullptr || (y + i) == nullptr ||
-				(w + i) == nullptr || (p + i) == nullptr ) {
-			d = 0.0;
-			break;
-		}
-		else {
-			d += *(w + i) * pow( abs( *(x + i) - *(y + i) ), *(p + i) );
-		}
-	}
+  for( i = 0; i < size ; i++ ) {
+    if ( (x + i) == nullptr || (y + i) == nullptr ||
+        (w + i) == nullptr || (p + i) == nullptr ) {
+      d = 0.0;
+      break;
+    }
+    else {
+      d += *(w + i) * pow( abs( *(x + i) - *(y + i) ), *(p + i) );
+    }
+  }
 
-	return d;
+  return d;
 }
 
 template< size_t N >
 double dst_weighted( double (&x)[N], double (&y)[N],
-		const double& w, const double& p ) {
+    const double& w, const double& p ) {
 
-	double d = 0.0;
-	int size_x = countof( x );
-	int size_y = countof( y );
+  double d = 0.0;
+  int size_x = countof( x );
+  int size_y = countof( y );
 
-	if ( size_x > 0 && size_x == size_y ) {
-		int i;
-		for( i = 0; i < size_x; i++ ) {
-			d += pow( abs( x[i] - y[i] ), p );
-		}
-	}
-	return pow( abs( w * d ), 1.0 / p );
+  if ( size_x > 0 && size_x == size_y ) {
+    int i;
+    for( i = 0; i < size_x; i++ ) {
+      d += pow( abs( x[i] - y[i] ), p );
+    }
+  }
+  return pow( abs( w * d ), 1.0 / p );
 }
 
 double dst_weighted( const int& size, double* x, double* y,
-		const double& w, const double& p ) {
+    const double& w, const double& p ) {
 
-	double d = 0.0;
-	int i;
-	for( i = 0; i < size; i++ ) {
-		if ( (x + i) == nullptr || (y + i) == nullptr ) {
-			d = 0.0;
-			break;
-		}
-		else {
-			d += pow( abs( *(x + i) - *(y + i) ), p );
-		}
-	}
+  double d = 0.0;
+  int i;
+  for( i = 0; i < size; i++ ) {
+    if ( (x + i) == nullptr || (y + i) == nullptr ) {
+      d = 0.0;
+      break;
+    }
+    else {
+      d += pow( abs( *(x + i) - *(y + i) ), p );
+    }
+  }
 
-	return pow( abs( w * d ), 1.0 / p );
+  return pow( abs( w * d ), 1.0 / p );
 }
 
 } // namespace Kriga
